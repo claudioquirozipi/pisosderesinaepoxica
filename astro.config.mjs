f6e8737abf9f771b1fe17d502b41dbfb5c29c62e
Claudio Quiroz <claudioquirozipi@gmail.com>
2023-11-10 00:29:12 -0500

    bugfix analytics with vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+// import vercel from "@astrojs/vercel/serverless";
+
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
@@ -13,4 +15,5 @@ export default defineConfig({
       enabled: true,
     },
   }),
+  // adapter: vercel()
 });
