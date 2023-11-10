f068bb6c4bf5dcae88bcc35e214318d3e9c2259c
Claudio Quiroz <claudioquirozipi@gmail.com>
2023-11-10 00:09:13 -0500

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.pisosderesinaepoxica.com/",
   integrations: [tailwind(), sitemap()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
