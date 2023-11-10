d0354450f160ef3a2c42e4fac239e5223720e720
Claudio Quiroz <claudioquirozipi@gmail.com>
2023-11-09 19:24:06 -0500

    add resins and component list and card

---
+++ src/pages/resinas/resins.ts
@@ -0,0 +1,40 @@
+export interface Resin {
+  title: string;
+  description: string;
+  price: number;
+  brand: string;
+  img: string[];
+  purchaseLink: string;
+}
+export const resins: Resin[] = [
+  {
+    title: "Resina epoxi transparente ",
+    description:
+      "Bonito resultado: Diseñada para que se vea hermosa y sea fuerte y duradera. Transparente. Brillante. Resplandeciente. Gruesa. Sólida. Dura. Profesional. La resina para manualidades mejora el atractivo y el valor de cualquier objeto sobre el que se aplique",
+    price: 69.99,
+    brand: "Craft Resin",
+    img: ["https://m.media-amazon.com/images/I/81PoPddaHqL._SL1500_.jpg"],
+    purchaseLink:
+      "https://www.amazon.com/transparente-revestimientos-art%C3%ADsticos-profesional-manualidades/dp/B083SRX7TJ/ref=sr_1_1_sspa?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=36E2CENWWV6A0&keywords=resinas%2Bepoxicas&qid=1699570866&sprefix=resinas%2Bepoxicas%2Caps%2C149&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1",
+  },
+  {
+    title: "Kit de resina epoxi de 2 galones",
+    description:
+      "Proyectos de bricolaje personalizados para uso doméstico o comercial: crea un trabajo personalizado con infinitas posibilidades; perfecto para proyectos de bricolaje en casa o para uso comercial/industrial; encimeras de abrigo, bar, mesas, mesas de río, mesas de café, joyas, artes y manualidades y posibilidades creativas casi infinitas. También es perfecto para llenar grietas, huecos o usarlo como un adhesivo de unión similar al pegamento",
+    price: 129.99,
+    img: ["https://m.media-amazon.com/images/I/6108WZcfrJL._SL1500_.jpg"],
+    brand: "East Coast Resin",
+    purchaseLink:
+      "https://www.amazon.com/-/es/resina-epoxi-galones-Resina-revestimientos/dp/B0BN71PDG7/ref=sr_1_4?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1S1CZD8ZYCEEJ&keywords=resinas+epoxicas+para+pisos&qid=1699571921&sprefix=resina+epoxica+para+pisos+%2Caps%2C164&sr=8-4",
+  },
+  {
+    title: "TAKIMO Kit de resina resistente a los rayos UV de 1 galón",
+    description:
+      " Fácil de usar y secado más rápido: nuestro kit incluye resina transparente de 0.5 galones y endurecedor transparente de 0.5 galones. Siempre que mezcle una relación 1:1 por volumen de la resina y el endurecedor, se seca rápidamente en 8 a 24 horas. No requiere experiencia, incluso si eres nuevo en la resina. Para lograr un acabado perfecto en climas fríos: resina precaliente y endurecedor antes de usar.",
+    price: 49.99,
+    img: ["https://m.media-amazon.com/images/I/716DIdvC--L._SL1500_.jpg"],
+    brand: "TAKIMO",
+    purchaseLink:
+      "https://www.amazon.com/-/es/resistente-transparente-revestimiento-fundici%C3%B3n-bricolaje/dp/B0C59L84CW/ref=sr_1_5?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1S1CZD8ZYCEEJ&keywords=resinas+epoxicas+para+pisos&qid=1699572089&sprefix=resina+epoxica+para+pisos+%2Caps%2C164&sr=8-5",
+  },
+];
